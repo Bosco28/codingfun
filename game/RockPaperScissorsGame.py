e54754dd04241bb733b3191d18dba2c308aa03dc
Bosco28 <boscpan@gmail.com>
2018-08-18 21:06:09 -0700

    Added comments for functions

--- game/RockPaperScissorsGame.py
+++ game/RockPaperScissorsGame.py
@@ -3,9 +3,9 @@ from GameInputException import GameInputException
 
 
 class RockPaperScissors:
-    # -1 if player1 loses
-    # 0 if players tie
-    # 1 if player1 wins
+    # return -1 if player1 loses
+    # return 0 if players tie
+    # return  1 if player1 wins
     def judge(self, player1, player2):
         if self.is_valid_input(player1) and self.is_valid_input(player2):
             if player1 == player2:
@@ -28,6 +28,7 @@ class RockPaperScissors:
         else:
             raise GameInputException("Invalid Input. Please try again.\n")
 
+    # return one of R, P or S randomly
     @staticmethod
     def generate_input():
         random = randint(1, 3)
@@ -38,6 +39,7 @@ class RockPaperScissors:
         else:
             return 'S'
 
+    # return true if input is one of R, P or S
     @staticmethod
     def is_valid_input(a):
         return a != '' and a in "RPS"
